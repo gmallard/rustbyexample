47f8d67644e6469768830d4915244a95b6e4b5fa
gmallard <allard.guy.m@gmail.com>
2016-03-03 15:20:32 -0500

    Example 01 - helloworld

---
+++ 01-helloworld/hello.rs
@@ -0,0 +1,13 @@
+// This is a comment, and will be ignored by the compiler
+
+// This code is editable, feel free to hack it!
+
+// This is the main function
+fn main() {
+    // The statements here will be executed when the compiled binary is called
+
+    // Print text to the console
+    println!("Hello World!");
+    println!("I'm a Rustacean!");
+}
+
