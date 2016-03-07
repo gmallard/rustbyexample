92d0fdbaaccca6f44e7313d54ea2dc6f00e88226
gmallard <allard.guy.m@gmail.com>
2016-03-06 21:39:29 -0500

    Add literals example.

---
+++ 05-casting/literals/src/main.rs
@@ -0,0 +1,27 @@
+/*
+Numeric literals can be type annotated by adding the type as a suffix, 
+with the exception of usize that uses the usize suffix and isize that 
+uses the isize suffix.
+
+The type of unsuffixed numeric literals will depend on how they are used. 
+If no constraint exists, the compiler will use i32 for integers, and f64 
+for floating-point numbers.
+*/
+
+fn main() {
+    // Suffixed literals, their types are known at initialization
+    let x = 1u8;
+    let y = 2u32;
+    let z = 3f32;
+
+    // Unsuffixed literal, their types depend on how they are used
+    let i = 1;
+    let f = 1.0;
+
+    // `size_of_val` returns the size of a variable in bytes
+    println!("size of `x` in bytes: {}", std::mem::size_of_val(&x));
+    println!("size of `y` in bytes: {}", std::mem::size_of_val(&y));
+    println!("size of `z` in bytes: {}", std::mem::size_of_val(&z));
+    println!("size of `i` in bytes: {}", std::mem::size_of_val(&i));
+    println!("size of `f` in bytes: {}", std::mem::size_of_val(&f));
+}
