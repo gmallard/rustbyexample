26761b1892ae388c7a0dbaa4e5d93aed2d61ebc3
gmallard <allard.guy.m@gmail.com>
2016-03-08 12:48:38 -0500

    Add attributes / cfg example.

---
+++ 12-attributes/cfg/src/main.rs
@@ -0,0 +1,31 @@
+/*
+Conditional compilation is possible through two different operators:
+
+1) the cfg attribute: #[cfg(...)] in attribute position
+2) the cfg! macro: cfg!(...) in boolean expressions
+
+Both utilize identical argument syntax.
+*/
+// This function only gets compiled if the target OS is linux
+#[cfg(target_os = "linux")]
+fn are_you_on_linux() {
+    println!("You are running linux!")
+}
+
+// And this function only gets compiled if the target OS is *not* linux
+#[cfg(not(target_os = "linux"))]
+fn are_you_on_linux() {
+    println!("You are *not* running linux!")
+}
+
+fn main() {
+    are_you_on_linux();
+
+    println!("Are you sure?");
+    if cfg!(target_os = "linux") {
+        println!("Yes. It's definitely linux!");
+    } else {
+        println!("Yes. It's definitely *not* linux!");
+    }
+}
+
