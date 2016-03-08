b0ad431a755f3a799466e5bcbf2e7fa27d9547bd
gmallard <allard.guy.m@gmail.com>
2016-03-07 20:19:15 -0500

    Embellish closures / iteratorfind example.

--- 08-functions/closures/examplesinstd/iteratorfind/src/main.rs
+++ 08-functions/closures/examplesinstd/iteratorfind/src/main.rs
@@ -27,15 +27,20 @@ fn main() {
 
     // A reference to what is yielded is `&&i32`. Destructure to `i32`.
     println!("Find 2 in vec1: {:?}", iter     .find(|&&x| x == 2));
+    println!("Find 5 in vec1: {:?}", iter     .find(|&&x| x == 5));
     // A reference to what is yielded is `&i32`. Destructure to `i32`.
+    // (I do not understand the results here ..... ?????)
     println!("Find 2 in vec2: {:?}", into_iter.find(| &x| x == 2));
+    println!("Find 5 in vec2: {:?}", into_iter.find(| &x| x == 5));
 
     let array1 = [1, 2, 3];
     let array2 = [4, 5, 6];
 
     // `iter()` for arrays yields `&i32`
     println!("Find 2 in array1: {:?}", array1.iter()     .find(|&&x| x == 2));
+    println!("Find 5 in array1: {:?}", array1.iter()     .find(|&&x| x == 5));
     // `into_iter()` for arrays unusually yields `&i32`
     println!("Find 2 in array2: {:?}", array2.into_iter().find(|&&x| x == 2));
+    println!("Find 5 in array2: {:?}", array2.into_iter().find(|&&x| x == 5));
 }
 
