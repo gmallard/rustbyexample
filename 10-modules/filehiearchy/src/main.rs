4a9270bff86a0a7e88bd283d2c5ce06a15159707
gmallard <allard.guy.m@gmail.com>
2016-03-08 11:37:20 -0500

    Add modules / filehierarchy example.

---
+++ 10-modules/filehiearchy/src/main.rs
@@ -0,0 +1,52 @@
+/*
+Modules can be mapped to a file/directory hierarchy. Let's break down the 
+visibility example in files:
+
+$ tree .
+.
+|-- my
+|   |-- inaccessible.rs
+|   |-- mod.rs
+|   `-- nested.rs
+`-- split.rs
+
+// split.rs
+// This declaration will look for a file named `my.rs` or `my/mod.rs` and will
+// insert its contents inside a module named `my` under this scope
+mod my;
+
+fn function() {
+    println!("called `function()`");
+}
+
+fn main() {
+    my::function();
+
+    function();
+
+    my::indirect_access();
+
+    my::nested::function();
+}
+
+*/
+
+// split.rs
+// This declaration will look for a file named `my.rs` or `my/mod.rs` and will
+// insert its contents inside a module named `my` under this scope
+mod my;
+
+fn function() {
+    println!("called `function()`");
+}
+
+fn main() {
+    my::function();
+
+    function();
+
+    my::indirect_access();
+
+    my::nested::function();
+}
+
